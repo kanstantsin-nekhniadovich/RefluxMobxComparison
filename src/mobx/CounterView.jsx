a454d251203b8da21f7a1faed24bf9c1b195a06f
kanstantsin-nekhniadovich <40122134+kanstantsin-nekhniadovich@users.noreply.github.com>
2021-04-24 23:01:46 -0700

    feature: use functional component (#9)

--- src/mobx/CounterView.jsx
+++ src/mobx/CounterView.jsx
@@ -1,42 +1,73 @@
 import React from 'react';
 import { observer, inject } from 'mobx-react';
 
-import { withStore } from './StoreProvider';
+import { withStore, useStore } from './StoreProvider';
 
-@withStore
-@observer
-class CounterView extends React.Component {
-  constructor() {
-    super();
-    this.decrementCounter = this.decrementCounter.bind(this);
-    this.incrementCounter = this.incrementCounter.bind(this);
-    this.setRandomCounterValue = this.setRandomCounterValue.bind(this);
+// use functional component approach
+export default observer(() => {
+  const store = useStore();
+
+  const decrementCounter = () => {
+    store.decrementCounter();
   }
 
-  decrementCounter() {
-    this.props.store.decrementCounter();
+  const incrementCounter = () => {
+    store.incrementCounter();
   }
 
-  incrementCounter() {
-    this.props.store.incrementCounter();
+  const setRandomCounterValue = () => {
+    store.setCounter(Math.floor(10 * Math.random()));
   }
 
-  setRandomCounterValue() {
-    this.props.store.setCounter(Math.floor(10 * Math.random()));
-  }
-
-  render() {
-    return (
-      <div className="counter-container">
-        <p>Counter: {this.props.store.counter}</p>
-        <div>
-          <button onClick={this.decrementCounter}>-</button>
-          <button onClick={this.incrementCounter}>+</button>
-          <button onClick={this.setRandomCounterValue}>Set counter</button>
-        </div>
+  return (
+    <div className="counter-container">
+      <p>Counter: {store.counter}</p>
+      <div>
+        <button onClick={decrementCounter}>-</button>
+        <button onClick={incrementCounter}>+</button>
+        <button onClick={setRandomCounterValue}>Set counter</button>
       </div>
-    );
-  }
-};
+    </div>
+  );
 
-export default CounterView;
+});
+
+// use class component approach
+
+// @withStore
+// @observer
+// class CounterView extends React.Component {
+//   constructor() {
+//     super();
+//     this.decrementCounter = this.decrementCounter.bind(this);
+//     this.incrementCounter = this.incrementCounter.bind(this);
+//     this.setRandomCounterValue = this.setRandomCounterValue.bind(this);
+//   }
+
+//   decrementCounter() {
+//     this.props.store.decrementCounter();
+//   }
+
+//   incrementCounter() {
+//     this.props.store.incrementCounter();
+//   }
+
+//   setRandomCounterValue() {
+//     this.props.store.setCounter(Math.floor(10 * Math.random()));
+//   }
+
+//   render() {
+//     return (
+//       <div className="counter-container">
+//         <p>Counter: {this.props.store.counter}</p>
+//         <div>
+//           <button onClick={this.decrementCounter}>-</button>
+//           <button onClick={this.incrementCounter}>+</button>
+//           <button onClick={this.setRandomCounterValue}>Set counter</button>
+//         </div>
+//       </div>
+//     );
+//   }
+// };
+
+// export default CounterView;
